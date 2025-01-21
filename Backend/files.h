4d62a6029f7169444f231d36329085037895c59e
Sam Camilleri <camillerisam1@gmail.com>
2025-01-21 19:41:40 +0000

    refactor: Added file handler header

---
+++ Backend/files.h
@@ -0,0 +1,18 @@
+/* 
+HTTP Server with User Authentication - Sam Camilleri
+File reader Header
+*/
+#ifndef HANDLERS_H
+#define HANDLERS_H
+
+#include <winsock2.h>
+
+// Maximum buffer size for request handling
+#define MAX_BUFFER_SIZE 4096
+
+// Handles incoming client connections
+// Parameters:
+// - clientSocket: Socket representing the client connection
+void clientHandler(SOCKET clientSocket);
+
+#endif // HANDLERS_H
