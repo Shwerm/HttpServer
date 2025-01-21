e8f3d06d76cb731bc076ac5e5dc432e018fed125
Sam Camilleri <camillerisam1@gmail.com>
2025-01-21 19:38:21 +0000

    feat: Added server header file and updated file description comments

---
+++ Backend/server.h
@@ -0,0 +1,14 @@
+/* 
+HTTP Server with User Authentication - Sam Camilleri
+Server Header
+*/
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+// Starts the HTTP server
+void startServer();
+
+#endif // SERVER_H
